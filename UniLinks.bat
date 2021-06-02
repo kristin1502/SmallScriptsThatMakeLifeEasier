e9ae5383cceba85919631f24c3183b6fca1907bd
Kristin <kristin1502.github@gmail.com>
2021-06-02 13:01:56 +0200

    Open links on start
    This script opens links on autostart.
    You can just paste a link to open after 'start "" '

---
+++ UniLinks.bat
@@ -0,0 +1,10 @@
+@echo off
+ECHO Opening Links:
+start "" https://www.moodle.tum.de/
+start "" https://mail.google.com/mail/u/0/#inbox
+start "" https://calendar.google.com/calendar/u/0/r/week
+
+ECHO Have a productive day!
+ECHO.
+pause
+exit
