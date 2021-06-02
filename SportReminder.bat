2be0b1d7554e064777d9a22195c31bbecf710310
Kristin <kristin1502.github@gmail.com>
2021-06-02 12:54:05 +0200

    Sport - Reminder
    I let this script run via the windows Task-Scheduler every hour to be reminded that I should move.
    Here is a tutorial: https://www.howtogeek.com/136894/how-to-create-popup-reminders-with-no-additional-software/

---
+++ SportReminder.bat
@@ -0,0 +1,19 @@
+@echo off
+ECHO SPORT - DU SCHAFFST DAS!
+ECHO.
+ECHO 10 Squats
+pause
+ECHO.
+ECHO 10 Push-Ups
+pause
+ECHO.
+ECHO 10 Sit-Ups
+pause
+ECHO.
+ECHO 30 Hampelmaenner
+pause 
+ECHO.
+ECHO GUT GEMACHT!
+ECHO.
+pause
+exit
