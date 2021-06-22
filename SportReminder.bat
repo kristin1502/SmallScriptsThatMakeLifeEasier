5f232979b299d3cffc5909b2171e85814ec71e54
Kristin <kristin1502.github@gmail.com>
2021-06-22 17:16:11 +0200

    SportReminder wählt aus mehreren möglichen Übungen

--- SportReminder.bat
+++ SportReminder.bat
@@ -1,13 +1,50 @@
 @echo off
+:: ================================================================
+:: Das Script erinnert dich daran Sport zu machen. 
+:: Passe die Übungen und Wiederholungen für dich persönlich an.
+:: ================================================================
+
+:: Squat Varianten
+set squatVariants[0]=Basic Squats
+set squatVariants[1]=Side Kick Squats
+set squatVariants[2]=Plie Squats
+set squatVariants[3]=Prisoner Squats
+set squatVariants[4]=Einbeinige Squats, 5 pro Seite
+
+:: Push-Up Varianten
+set pushUpVariants[0]=Basic Push-Ups
+set pushUpVariants[1]=Diamant Push-Ups
+set pushUpVariants[2]=Wide Hands Push-Ups
+set pushUpVariants[3]=Gestaffelter Push-Ups
+set pushUpVariants[4]=Kniender Push-Up
+
+:: Sit-Up Varianten
+set sitUpVariants[0]=Basic Sit-Ups
+set sitUpVariants[1]=Crunches
+set sitUpVariants[2]=Russian Twists
+set sitUpVariants[3]=Bicycle Crunches
+set sitUpVariants[4]=Toe Touches
+
+:: Random Varianten auswählen, 2x generieren, da oft die gleichen Werte bei erneutem Ausführen auftreten idk
+set min=0
+set max=4
+set /a randomSquat=min+(max-min+1)*%random%/32768
+set /a randomSquat=min+(max-min+1)*%random%/32768
+set /a randomPushUp=min+(max-min+1)*%random%/32768
+set /a randomPushUp=min+(max-min+1)*%random%/32768
+set /a randomSitUp=min+(max-min+1)*%random%/32768
+set /a randomSitUp=min+(max-min+1)*%random%/32768
+
+setlocal enableDelayedExpansion
 ECHO SPORT - DU SCHAFFST DAS!
 ECHO.
-ECHO 10 Squats
+ECHO 10 !squatVariants[%randomSquat%]!
 pause
 ECHO.
-ECHO 10 Push-Ups
+ECHO 10 !pushUpVariants[%randomPushUp%]!
 pause
 ECHO.
-ECHO 10 Sit-Ups
+ECHO 10 !sitUpVariants[%randomSitUp%]!
 pause
 ECHO.
 ECHO 30 Hampelmaenner
@@ -16,4 +53,6 @@ ECHO.
 ECHO GUT GEMACHT!
 ECHO.
 pause
+
+endlocal
 exit
